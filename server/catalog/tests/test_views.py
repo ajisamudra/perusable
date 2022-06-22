c9c70c0d3451a481585a8696efb95e6b68dae91e
ajisamudra <aji.samudra@traveloka.com>
2022-06-22 22:03:46 +0700

    add ranking

--- server/catalog/tests/test_views.py
+++ server/catalog/tests/test_views.py
@@ -27,21 +27,26 @@ class ViewTests(APITestCase):
 
     def test_query_matches_description(self):
         response = self.client.get("/api/v1/catalog/wines/?query=wine")
-        self.assertEquals(3, len(response.data))
+        self.assertEquals(4, len(response.data))
         self.assertCountEqual(
             [
                 "58ba903f-85ff-45c2-9bac-6d0732544841",
                 "21e40285-cec8-417c-9a26-4f6748b7fa3a",
                 "0082f217-3300-405b-abc6-3adcbecffd67",
+                "000bbdff-30fc-4897-81c1-7947e11e6d1a",
             ],
             [item["id"] for item in response.data],
         )
 
     def test_query_matches_country(self):
         response = self.client.get("/api/v1/catalog/wines/?country=France")
-        self.assertEquals(1, len(response.data))
-        self.assertEquals(
-            "0082f217-3300-405b-abc6-3adcbecffd67", response.data[0]["id"]
+        self.assertEquals(2, len(response.data))
+        self.assertCountEqual(
+            [
+                "0082f217-3300-405b-abc6-3adcbecffd67",
+                "000bbdff-30fc-4897-81c1-7947e11e6d1a",
+            ],
+            [item["id"] for item in response.data],
         )
 
     def test_query_matches_points(self):
@@ -51,7 +56,24 @@ class ViewTests(APITestCase):
             "21e40285-cec8-417c-9a26-4f6748b7fa3a", response.data[0]["id"]
         )
 
-    def test_country_must_be_exact_match(self):
-        response = self.client.get("/api/v1/catalog/wines/?country=Frances")
-        self.assertEquals(0, len(response.data))
-        self.assertJSONEqual(response.content, [])
+    def test_can_filter_on_country(self):
+        response = self.client.get("/api/v1/catalog/wines/?country=France")
+        self.assertEquals(2, len(response.data))
+        self.assertCountEqual(
+            [
+                "0082f217-3300-405b-abc6-3adcbecffd67",
+                "000bbdff-30fc-4897-81c1-7947e11e6d1a",
+            ],
+            [item["id"] for item in response.data],
+        )
+
+    def test_search_result_returned_in_correct_order(self):
+        response = self.client.get("/api/v1/catalog/wines/?query=Chardonnay")
+        self.assertEquals(2, len(response.data))
+        self.assertListEqual(
+            [
+                "0082f217-3300-405b-abc6-3adcbecffd67",
+                "000bbdff-30fc-4897-81c1-7947e11e6d1a",
+            ],
+            [item["id"] for item in response.data],
+        )
