57246883b01221de545be1398826226e19907c00
ajisamudra <aji.samudra@traveloka.com>
2022-06-25 11:23:50 +0700

    search client

--- client/cypress/integration/all.spec.js
+++ client/cypress/integration/all.spec.js
@@ -4,3 +4,10 @@ describe('Perusable', function () {
       cy.get('h1').should('contain', 'Perusable');
     });
   });
+
+it('Displays a list of results.', function () {
+cy.visit('/');
+cy.get('input#query').type('cabernet');
+cy.get('button').contains('Search').click();
+cy.get('div.card-title').should('contain', 'Cabernet Sauvignon');
+});
