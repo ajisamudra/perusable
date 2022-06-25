6e1cd517949b2120791652ff811af34b3ad212eb
ajisamudra <aji.samudra@traveloka.com>
2022-06-25 11:50:57 +0700

    add typeahead for query suggestion

--- client/cypress/integration/all.spec.js
+++ client/cypress/integration/all.spec.js
@@ -11,8 +11,23 @@ it('Displays a list of results.', function () {
   cy.visit('/');
   cy.get('input#country').type('US');
   cy.get('input#points').type('92');
-  cy.get('input#query').type('staglin');
+  cy.get('input[placeholder="Enter a search term (e.g. cabernet)"]')
+    .type('cabernet');
   cy.get('button').contains('Search').click();
   cy.wait('@getWines');
   cy.get('div.card-title').should('contain', 'Cabernet Sauvignon');
+});
+
+it('Displays wine search words.', function () {
+  // Stub server
+  cy.intercept(
+    'GET', '**/api/v1/catalog/wine-search-words/**',
+    { fixture: 'wine_search_words.json' }
+  ).as('getWineSearchWords');
+
+  cy.visit('/');
+  cy.get('input[placeholder="Enter a search term (e.g. cabernet)"]')
+    .type('cabarnet');
+  cy.wait('@getWineSearchWords');
+  cy.get('div#query').should('contain', 'cabernet');
 });
